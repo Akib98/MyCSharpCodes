013f980dc95cf58a5c62f35b55f00205fa4a966d
Akib Ibn Zahid <akibibnzahid@gmail.com>
2018-03-05 11:31:55 +0600

    First C# code

---
+++ MyCSharpProject/MyCSharpProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCSharpProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
