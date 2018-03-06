b3c4d11f60f2d702c84b6e40b8c2dbf7378fd90b
Akib Ibn Zahid <akibibnzahid@gmail.com>
2018-03-06 17:31:56 +0600

    GCD of five numbers

---
+++ MyCSharpProject/MyCSharpProject/MathFile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCSharpProject
+{
+    public class MathFile
+    {
+        public static int GetGCDofNumbers(int[] numbers)
+        {
+            int gcd = 1;
+            int index = 2;
+            if(numbers.Length == 1)
+            {
+                gcd = numbers[1];
+            }
+
+            if(numbers.Length > 1)
+            {
+                gcd = GCDofTwoNumbers(numbers[1], numbers[2]);
+            }
+
+            while(index < numbers.Length)
+            {
+                gcd = GCDofTwoNumbers(gcd, numbers[index]);
+                index++;
+            }
+
+            return gcd;
+        }
+
+        private static int GCDofTwoNumbers(int number1, int number2)
+        {
+            if (number2 != 0)
+                return GCDofTwoNumbers(number2, number1 % number2);
+            else
+                return number1;
+        }
+    }
+    
+}
--- MyCSharpProject/MyCSharpProject/Program.cs
+++ MyCSharpProject/MyCSharpProject/Program.cs
@@ -10,24 +10,37 @@ namespace MyCSharpProject
     {
         static void Main()
         {
+            #region GCD of five numbers
+            Console.WriteLine("Input five numbers and get the GCD: ");
+            int[] newArray = new int[5];
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                newArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int result = MathFile.GetGCDofNumbers(newArray);
+
+            Console.Write("The GCD of these numbers is: ");
+            Console.WriteLine(result);
+            #endregion
+
             #region Anagram Check
-            
-            Console.WriteLine("Check two Strings are anagram or not!!");
-            Console.WriteLine("Enter first String: ");
-            string firstString = Console.ReadLine();
-            Console.WriteLine("Enter second string: ");
-            string secondString = Console.ReadLine();
 
-            bool IsAnagram = StringFile.isAnagram(firstString, secondString);
+            //Console.WriteLine("Check two Strings are anagram or not!!");
+            //Console.WriteLine("Enter first String: ");
+            //string firstString = Console.ReadLine();
+            //Console.WriteLine("Enter second string: ");
+            //string secondString = Console.ReadLine();
 
-            if(IsAnagram)
-                Console.WriteLine("Two strings are Anagram");
-            else
-                Console.WriteLine("Two strings are not Anagram!");
+            //bool IsAnagram = StringFile.isAnagram(firstString, secondString);
+
+            //if(IsAnagram)
+            //    Console.WriteLine("Two strings are Anagram");
+            //else
+            //    Console.WriteLine("Two strings are not Anagram!");
 
             #endregion
+
             Console.ReadKey();
-        }
-        
+        }   
     }
 }
